949d0269b33415349e94a71c662ebb31b4508940
KHJ <rlagurwnsid2@gmail.com>
2023-09-07 16:55:43 +0900

    first

---
+++ src/main/java/com/example/demo/test/TestController.java
@@ -0,0 +1,21 @@
+package com.example.demo.test;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@RequiredArgsConstructor
+public class TestController {
+
+  @GetMapping("/all")
+  public ResponseEntity<?> findAll(){
+    return ResponseEntity.status(HttpStatus.OK).body("hello world");
+  }
+}
